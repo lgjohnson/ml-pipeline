bc940103c898b119b111a587b63c241af0721117
lgjohnson <gregleejohnson@gmail.com>
2019-06-12 10:56:01 -0700

    broke logging and lambda bucket out into global folder

--- tf/stg/pipeline/s3.tf
+++ tf/stg/pipeline/s3.tf
@@ -1,9 +1,3 @@
-#s3 bucket for ml pipeline lambdas
-resource "aws_s3_bucket" "lambdas_bucket" {
-  bucket = "lgjohnson-ml-pipeline-lambda-${var.stack_env}"
-  acl    = "private"
-}
-
 #s3 bucket for training data
 resource "aws_s3_bucket" "training_bucket" {
     bucket  = "lgjohnson-ml-pipeline-training-${var.stack_env}"
@@ -21,10 +15,4 @@ resource "aws_s3_bucket" "training_bucket" {
     tags = {
         Environment = "Staging"
     }
-}
-
-#s3 bucket for logs
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "lgjohnson-ml-pipeline-logs-${var.stack_env}"
-  acl    = "log-delivery-write"
 }
