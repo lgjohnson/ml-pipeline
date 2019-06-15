1f366c4f8d29128dcccb80d7030feeaebe50c053
lgjohnson <gregleejohnson@gmail.com>
2019-06-15 13:33:08 -0700

    script to upload airline data to training pipeline

--- scripts/upload_training_data.sh
+++ scripts/upload_training_data.sh
@@ -2,19 +2,30 @@
 set -eo pipefail
 
 # Usage:
-#   uploads a jpg image to the training bucket, triggering the lambda job
-#   specify the environment and the path to the image as respective arguments
+#   uploads "Reporting Carrier On-Time Performance" dataset to the ml-pipeline training bucket
+#   specifically, uploads separates CSVs for the years 1987 to 2007
+#   more info here: http://stat-computing.org/dataexpo/2009/the-data.html
+#   takes one mandatory argument, the environment: "stg" or "prod"
 # Example:
-#   ./upload_training_data.sh stg path/to/image.jpg
+#   ./upload_training_data.sh stg
 
 STACK_ENV=$1
-IMAGE_PATH=$2
-FILENAME=$(basename $IMAGE_PATH)
 
 S3_TRAINING_BUCKET="lgjohnson-ml-pipeline-training-${STACK_ENV}"
 S3_TRAINING_PREFIX="training_data"
-LAMBDA_S3_URI="s3://${S3_TRAINING_BUCKET}/${S3_TRAINING_PREFIX}/${FILENAME}"
+LAMBDA_S3_URI="s3://${S3_TRAINING_BUCKET}/${S3_TRAINING_PREFIX}"
 
-aws s3 cp --quiet $IMAGE_PATH $LAMBDA_S3_URI
+TEMP_FOLDER="/tmp"
+START_YEAR=1987
+END_YEAR=2007
 
-echo "${FILENAME} successfully uploaded to ${LAMBDA_S3_URI}."
+for YEAR in $(seq $START_YEAR $END_YEAR)
+    do
+        DOWNLOAD_URL="http://stat-computing.org/dataexpo/2009/${YEAR}.csv.bz2"
+        curl -s $DOWNLOAD_URL -o "${TEMP_FOLDER}/${YEAR}.csv.bz2"
+        bzip2 --force -d "${TEMP_FOLDER}/${YEAR}.csv.bz2"
+        aws s3 cp --quiet "${TEMP_FOLDER}/${YEAR}.csv" "${LAMBDA_S3_URI}/${YEAR}.csv"
+        echo "data for the year ${YEAR} was successfully uploaded to ${LAMBDA_S3_URI}/${YEAR}.csv"
+    done
+
+echo "all training data successfully uploaded"
